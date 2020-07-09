c02a7cf0cf4ed3496150e82a33ede1a284301467
Prajwal <prajwaljadhav248@gmail.com>
2020-07-09 18:30:28 +0530

    cc

--- app/src/main/java/com/pentagon/covidassisitant/MainActivity.java
+++ app/src/main/java/com/pentagon/covidassisitant/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTest, mChat, mDate, mTotal, mRecovered, mDead, mTextSwipe;
     private Button mMore;
     private GraphView mGraph;
+    private GraphView mGraphDaily;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         mRecovered = findViewById(R.id.activity_main_text_view_recovered);
         mDead = findViewById(R.id.activity_main_text_view_dead);
         mGraph = findViewById(R.id.activity_main_graph_view);
+        mGraphDaily = findViewById(R.id.activity_main_graph_view_daily);
         mMore = findViewById(R.id.activity_main_button_more);
         mMore.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,36 +111,75 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    LineGraphSeries<DataPoint> Total, Recovered, Dead;
+                    LineGraphSeries<DataPoint> Total, Recovered, Dead, Active, dConf, dDecr, dReco, dTotal;
                     Total = new LineGraphSeries<>();
                     Recovered = new LineGraphSeries<>();
                     Dead = new LineGraphSeries<>();
-                    int T, R, D;
+                    Active = new LineGraphSeries<>();
+
+                    dConf = new LineGraphSeries<>();
+                    dDecr = new LineGraphSeries<>();
+                    dReco = new LineGraphSeries<>();
+                    dTotal = new LineGraphSeries<>();
+                    int T, R, D, A, dC, dD, dR, dT;
                     JSONArray jsonArray = response.getJSONArray("cases_time_series");
-                    for (int i = 0; i<jsonArray.length(); i++){
-                        JSONObject object = jsonArray.getJSONObject(i);
-                        T = Integer.parseInt(object.getString("totalconfirmed"));
-                        D = Integer.parseInt(object.getString("totaldeceased"));
-                        R = Integer.parseInt(object.getString("totalrecovered"));
-                        Total.appendData(new DataPoint(i, T), true, 80);
-                        Recovered.appendData(new DataPoint(i, R), true, 80);
-                        Dead.appendData(new DataPoint(i, D), true, 80);
-                        if (i == jsonArray.length()-1){
-                            mDate.setText(object.getString("date"));
-                            mTotal.setText(object.getString("totalconfirmed"));
-                            mRecovered.setText(object.getString("totalrecovered"));
-                            mDead.setText(object.getString("totaldeceased"));
+                    for (int j = 0; j<jsonArray.length(); j++){
+                        //int i = j-jsonArray.length()-60;
+                        int dynamic_length = jsonArray.length();
+                        int i=j;
+                        if (true){
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            T = Integer.parseInt(object.getString("totalconfirmed"));
+                            D = Integer.parseInt(object.getString("totaldeceased"));
+                            R = Integer.parseInt(object.getString("totalrecovered"));
+                            A = Math.abs(T-D-R);
+
+                            dC = Integer.parseInt(object.getString("dailyconfirmed"));
+                            dD = Integer.parseInt(object.getString("dailydeceased"));
+                            dR = Integer.parseInt(object.getString("dailyrecovered"));
+                            dT = Math.abs(dC-dR-dD);
+                            Total.appendData(new DataPoint(i, T), true, dynamic_length);
+                            Recovered.appendData(new DataPoint(i, R), true, dynamic_length);
+                            Dead.appendData(new DataPoint(i, D), true, dynamic_length);
+                            Active.appendData(new DataPoint(i, A), true, dynamic_length);
+
+                            dConf.appendData(new DataPoint(i, dC), true, dynamic_length);
+                            dDecr.appendData(new DataPoint(i, dD), true, dynamic_length);
+                            dReco.appendData(new DataPoint(i, dR), true, dynamic_length);
+                            dTotal.appendData(new DataPoint(i, dT), true, dynamic_length);
+                            if (i == jsonArray.length()-1){
+                                mDate.setText(object.getString("date"));
+                                mTotal.setText(object.getString("totalconfirmed"));
+                                mRecovered.setText(object.getString("totalrecovered"));
+                                mDead.setText(object.getString("totaldeceased"));
+                            }
                         }
                     }
                     Total.setColor(Color.BLUE);
                     Recovered.setColor(Color.GREEN);
                     Dead.setColor(Color.RED);
+                    Active.setColor(Color.BLACK);
                     mGraph.getViewport().setMaxX(jsonArray.length());
                     mGraph.getViewport().setXAxisBoundsManual(true);
-                    mGraph.setTitle("Corona cases in india (30 Jan onwards)");
+                    mGraph.setTitle("Corona cases in india last 80 days");
                     mGraph.addSeries(Total);
                     mGraph.addSeries(Recovered);
                     mGraph.addSeries(Dead);
+                    mGraph.addSeries(Active);
+
+                    dDecr.setColor(Color.RED);
+                    dReco.setColor(Color.GREEN);
+                    dConf.setColor(Color.BLUE);
+                    dTotal.setColor(Color.BLACK);
+                    mGraphDaily.getViewport().setMaxX(jsonArray.length());
+                    mGraphDaily.getViewport().setXAxisBoundsManual(true);
+                    mGraphDaily.setTitle("Daily corona cases in india");
+                    mGraphDaily.addSeries(dDecr);
+                    mGraphDaily.addSeries(dReco);
+                    mGraphDaily.addSeries(dConf);
+                    mGraphDaily.addSeries(dTotal);
+
+
                 } catch (JSONException e) {
                     Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
